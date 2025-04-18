4ec3d75112b62eb094d9f936bee9de624729e204
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-04-18 19:30:41 +0200

    Add:section blog && ArticleBlog & Article

--- assets/scripts/blog.js
+++ assets/scripts/blog.js
@@ -2,7 +2,7 @@ const articles = {
   wadeWarren: {
     imgArtcale: "/assets/img/image 26.png",
     title: "Crochet Projects for Noodle Lovers",
-    description: "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    description: "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do <br/> eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
     clint: "Wade Warren",
     clintImg: "/assets/img/Ellipse 3.png",
     date: "12 November 2021",
@@ -10,7 +10,7 @@ const articles = {
   robertFox: {
     imgArtcale: "/assets/img/image 26-1.png",
     title: "10 Vegetarian Recipes To Eat This Month",
-    description: "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    description: "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do <br/> eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
     clint: "Robert Fox",
     clintImg: "/assets/img/Ellipse 2-1.png",
     date: "12 November 2021",
@@ -18,7 +18,7 @@ const articles = {
   dianneRussell: {
     imgArtcale: "/assets/img/image 26-2.png",
     title: "Full Guide to Becoming a Professional Chef",
-    description: "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    description: "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do <br/> eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
     clint: "Dianne Russell",
     clintImg: "/assets/img/Ellipse 2-2.png",
     date: "12 November 2021",
@@ -26,7 +26,7 @@ const articles = {
   leslieAlexander: {
     imgArtcale: "/assets/img/image 26-3.png",
     title: "Simple & Delicious Vegetarian Lasagna",
-    description: "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    description: "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do <br/> eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
     clint: "Leslie Alexander",
     clintImg: "/assets/img/Ellipse 2-3.png",
     date: "12 November 2021",
@@ -34,7 +34,7 @@ const articles = {
   courtneyHenry: {
     imgArtcale: "/assets/img/image 26.png",
     title: "Plantain and Pinto Stew with Aji Verde",
-    description: "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    description: "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do <br/> eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
     clint: "Courtney Henry",
     clintImg: "/assets/img/Ellipse 2-4.png",
     date: "12 November 2021",
@@ -42,7 +42,7 @@ const articles = {
   albertFlores: {
     imgArtcale: "/assets/img/image 26-4.png",
     title: "We’re Hiring a Communications Assistant!",
-    description: "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    description: "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do <br/> eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
     clint: "Albert Flores",
     clintImg: "/assets/img/Ellipse 2-5.png",
     date: "12 November 2021",
@@ -54,19 +54,21 @@ function blogAndArtices() {
     const content = document.createElement("div");
     content.classList.add("artice-items");
     content.innerHTML = `
-      <div> 
-        <div>
+        <div class="img-article">
         <img src="${article.imgArtcale}" alt="${article.title}">
         </div>
-        <div>
+        <div class="info"> 
         <h2>${article.title}</h2>
         <p>${article.description}</p>
-        <div>
+
+        <div class="info-clint">
+        <div class="clint">
         <img src="${article.clintImg}" alt=${article.clint}>
-        <span>${article.clint}</span>
+        <span>${article.clint}</span><span class="date-article">${article.date}</span>
         </div>
+
         </div>
-      </div>   
+        </div> 
     `;
     contentContainer.appendChild(content);
   });
